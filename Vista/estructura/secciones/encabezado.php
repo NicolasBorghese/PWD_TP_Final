9179576ef50a5d201fd5506f0d87c242f52bf1a1
NicolasBorghese <nicolasborghese@hotmail.com>
2023-11-11 22:21:16 -0300

    Modificaciones en la estructura de la vista

--- Vista/estructura/secciones/encabezado.php
+++ Vista/estructura/secciones/encabezado.php
@@ -29,22 +29,3 @@
 </head>
 
 <body>
-
-<header class="header">
-        <div class="menu-opciones">
-            <div class="logo-nav">
-                <img src="Archivos/Imagenes/logoBlanco.png">
-            </div>
-            <div class="navbar">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-            </div>
-            <div class="navbar"><a class="nav-link" href="#">Mates</a></div>
-            <div class="navbar"><a class="nav-link" href="#">Yerbas</a></div>
-            <div class="navbar"><a class="nav-link" href="#">Bombillas</a></div>
-            <div class="navbar"><a class="nav-link" href="#">Termos</a></div>
-            <div class="navbar"><a class="nav-link" href="#">SETS</a></div>
-        </div>
-        <div class="login">
-            <i class="bi bi-person-fill"></i>
-        </div>
-    </header>
---
+++ Vista/estructura/secciones/nav-bar-1.php
@@ -0,0 +1,22 @@
+<body onload="actualizarCurrent(<?php echo $seccionActual ?>)">
+
+<!-- ____________________________________ NAV BAR 1 ________________________________ -->
+<div id="contenedorCuerpo">
+<header class="header">
+    <div class="menu-opciones">
+        <div class="logo-nav">
+            <img src="Archivos/Imagenes/logoBlanco.png">
+        </div>
+        <div class="navbar">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </div>
+        <div class="navbar"><a class="nav-link" href="#">Mates</a></div>
+        <div class="navbar"><a class="nav-link" href="#">Yerbas</a></div>
+        <div class="navbar"><a class="nav-link" href="#">Bombillas</a></div>
+        <div class="navbar"><a class="nav-link" href="#">Termos</a></div>
+        <div class="navbar"><a class="nav-link" href="#">SETS</a></div>
+    </div>
+    <div class="login">
+        <i class="bi bi-person-fill"></i>
+    </div>
+</header>
