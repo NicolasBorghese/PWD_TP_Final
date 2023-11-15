764305fdfcda882938271c26cbfe11a1ac87d276
joaquinirigoyen <joaquin.iri25@gmail.com>
2023-11-15 18:54:41 -0300

    gestion menu

---
+++ Vista/administrador/gestionMenu.php
@@ -0,0 +1,45 @@
+<?php
+$tituloPagina = "TechnoMate | Administrador";
+include_once '../estructura/secciones/head.php';
+include("../estructura/secciones/nav-bar-1.php");
+require_once("../../Modelo/Conector/BaseDatos.php");
+include_once("../../configuracion.php");
+
+$objMenu = new AbmMenu();
+$listMenu = $objMenu->buscar(null);
+?>
+<div class="contenido-pagina">
+    <strong>Menus</strong>
+    <?php
+    if (count($listMenu)>0){
+        echo '<table class="table">
+        <thead >
+            <tr>  
+              <th><strong>IdMenu</strong></th>
+              <th><strong>menombre</strong></th>
+              <th><strong>medescripcion</strong></th>
+              <th><strong>idPadre Nacimiento</strong></th>
+              <th><strong>meDesabhilitado</strong></th>
+            </tr>
+        </thead>
+        <tbody>';
+        foreach($listMenu as $objM){
+            echo '<tr>';
+            echo '<td>'.$objM->getIdMenu().'</td>';
+            echo '<td>'.$objM->getMeNombre().'</td>';
+            echo '<td>'.$objM->getMeDescripcion().'</td>';
+            echo '<td>'.$objM->getMenuPadre().'</td>';
+            echo '<td>'.$objM->getMeDeshabilitado().'</td>';
+            echo '</tr>'; 
+        }
+        echo'</tbody>
+        </table>';      
+    }else{
+        echo '<h4>No se han cargado menus.</h4>';  
+        }
+    ?>
+</div>
+
+<?php
+include_once '../estructura/secciones/footer.php';
+?>
