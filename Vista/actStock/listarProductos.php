c79ace72ec6f69b8a98f7169f5ff1a6692d9407f
Alter1412 <florianalaforgia@hotmail.com.ar>
2023-11-16 07:12:16 -0300

    Vinculacion de modificar stock

--- Vista/actStock/listarProductos.php
+++ Vista/actStock/listarProductos.php
@@ -1,21 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    include_once '../../Control/AbmProducto.php';
-    include_once '../../Modelo/Conector/BaseDatos.php';
-    include_once '../../Modelo/Producto.php';
-   
+<?php
+include_once("../../configuracion.php");
+include_once '../../Control/AbmUsuario.php';
+include_once '../../Modelo/Conector/BaseDatos.php';
+include_once '../../Modelo/Usuario.php';
+
+$tituloPagina = "TechnoMate | Administrador";
+include_once '../estructura/secciones/head.php';
+
+$objSesion = new Session();
+
+if ($objSesion->validar()){
+    if($_SESSION['rol'] == 2){
+        include_once '../estructura/secciones/nav-bar-2.php';
+    } else {
+        header('Location: home.php');
+    }
+} else {
+    header('Location: home.php');
+}
+
+
     $objProducto = new AbmProducto();
     $colProductos = $objProducto->buscar("");
 
-    echo "<table border='1'>";
+?>
+
+<div class="container-fluid" style="padding: 50px;">
+
+<?php
+
+if (!empty($colProductos)){
+    echo "<h4>Listado de productoss</h4>";
+    echo "<table class='table table-striped table-hover'>";
     echo "<th>ID de producto</th>
     <th>Descripción</th>
     <th>Cantidad de stock</th>
@@ -26,10 +42,18 @@
         <td>".$producto->getIdProducto()."</td>
         <td>".$producto->getProNombre()."</td>
         <td>".$producto->getProCantstock()."</td>
-        <td><a href='formActualizar.php?idproducto=" . $producto->getIdProducto()."'>Actualizar</a></td>>
+        <td><button class='btn text-white btn-dark'><a style='text-decoration: none;' href='formActualizar.php?idproducto=" . $producto->getIdProducto()."'>Actualizar</a></button></td>>
         </tr>";
     }
 
-    ?>
-</body>
-</html>
+    echo "</table>";
+} else {
+    echo "<h4>No hay productos cargados en la Base de Datos";
+}
+  
+?>
+</div>
+
+<?php
+include_once '../estructura/secciones/footer.php';
+?>
