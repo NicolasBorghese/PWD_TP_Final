6a4c46a0e69ef4d79962564cbe3ad4962cbf727d
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2023-11-17 11:52:12 -0300

    Editar usuarios / Editar rol (pendiente)

--- Vista/administrador/homeAdministrador.php
+++ Vista/administrador/homeAdministrador.php
@@ -5,40 +5,34 @@ include_once("../estructura/headSeguro.php");
 include_once("../estructura/navSeguro.php");
 ?>
 
-<div class ="contenido-pagina">
+<div class="contenido-pagina">
     <div class="contenedor-acciones">
-        
+
         <div class="accion-admin">
-        <a data-bs-target="#modalNuevoUsuario" tabindex="-1" data-bs-toggle="modal">
-            <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin1.png" alt="Crear nuevo usuario">
-            <div class="informacion-accion">
-                <p>CREAR NUEVOS USUARIOS</p>
-            </div>
-            </a>
-        </div>
-        
-        <div class="accion-admin">
-            <a href ="../actInfoUsuarios/listarUsuarios.php">
-            <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin2.png" alt="Actualizar información de usuario">
-            <div class="informacion-accion">
-                <p>ACTUALIZAR INFORMACIÓN DE USUARIOS</p>
-            </div>
+            <a data-bs-target="#modalNuevoUsuario" tabindex="-1" data-bs-toggle="modal">
+                <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin1.png" alt="Crear nuevo usuario">
+                <div class="informacion-accion">
+                    <p>CREAR NUEVOS USUARIOS</p>
+                </div>
             </a>
         </div>
 
         <div class="accion-admin">
-            <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin3.png" alt="Administrar roles">
-            <div class="informacion-accion">
-                <p>ADMINISTRAR ROLES DE USUARIOS</p>
-            </div>
+            <a href="listarUsuarios.php">
+                <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin2.png"
+                    alt="Actualizar información de usuario">
+                <div class="informacion-accion">
+                    <p>ACTUALIZAR INFORMACIÓN DE USUARIOS</p>
+                </div>
+            </a>
         </div>
 
         <div class="accion-admin">
             <a href="../administrador/gestionMenu.php">
-            <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin4.png" alt="Crear nuevo rol">
-            <div class="informacion-accion">
-                <p>CREAR NUEVOS ROLES E ÍTEMS DE MENÚ</p>
-            </div>
+                <img class="imagen-accion" src="../../Archivos/Imagenes/accionAdmin3.png" alt="Crear nuevo rol">
+                <div class="informacion-accion">
+                    <p>CREAR NUEVOS ROLES E ÍTEMS DE MENÚ</p>
+                </div>
             </a>
         </div>
     </div>
--- Vista/administrador/listarUsuarios.php
+++ Vista/administrador/listarUsuarios.php
@@ -3,9 +3,14 @@ include_once("../../configuracion.php");
 include_once '../../Control/AbmUsuario.php';
 include_once '../../Modelo/Conector/BaseDatos.php';
 include_once '../../Modelo/Usuario.php';
+include_once '../../Control/AbmUsuarioRol.php';
+include_once '../../Control/AbmRol.php';
+include_once '../../Modelo/UsuarioRol.php';
+include_once '../../Modelo/Rol.php';
 
 $tituloPagina = "TechnoMate | Administrador";
-include_once '../estructura/secciones/head.php';
+include_once '../estructura/headSeguro.php';
+include_once '../estructura/navSeguro.php';
 
 $datos = data_submitted();
 
@@ -23,36 +28,46 @@ if ($objSesion->validar()){
 
 $objUsuario = new AbmUsuario();
 $colUsuarios = $objUsuario->buscar("");
+
+$objUsuarioRol = new AbmUsuarioRol();
+$colUsuarioRol = $objUsuarioRol->buscar("");
+
 ?>
 
 <div class="container-fluid" style="padding: 50px;">
 
-<?php
-if (!empty($colUsuarios)){
-    if(array_key_exists('exito', $datos)){
+    <?php
+    if (!empty($colUsuarioRol)){
+        if(array_key_exists('exito', $datos)){
         echo "<h3>Modificación realizada con éxito a: ".$datos['exito']."!</h3>";
     }
+    
     echo "<h4>Listado de usuarios</h4>";
     echo "<table class='table table-striped table-hover'>";
     echo "<th>#</th>
     <th>Nombre de Usuario</th>
-    <th>Acción</th>";
+    <th>Modificar</th>";
 
-    foreach($colUsuarios as $usuario){
+    foreach($colUsuarioRol as $usuarioRol){
         echo "<tr>
-        <td>".$usuario->getIdUsuario()."</td>
-        <td>".$usuario->getUsNombre()."</td>
-        <td><button class='btn text-white btn-dark'><a style='text-decoration: none;' href='formModificarUsuarios.php?idusuario=" . $usuario->getIdusuario() . "'>Modificar</a></button></td>
+        <td>".$usuarioRol->getObjUsuario()->getIdUsuario()."</td>
+        <td>".$usuarioRol->getObjUsuario()->getUsNombre()."</td>
+        <td><button class='btn text-white btn-dark' data-bs-toggle='modal' data-bs-target='#modalModificacion' tabindex='-1'><a style='text-decoration: none;' href='formModificarUsuarios.php?idusuario= ". $usuarioRol->getObjUsuario()->getIdusuario() . "'>Datos</a></button>
+        <button class='btn text-white btn-dark' data-bs-toggle='modal' data-bs-target='#modalModificacion' tabindex='-1'><a style='text-decoration: none;' href='formModificarRoles.php?idusuario= ". $usuarioRol->getObjUsuario()->getIdusuario() . "'>Roles</a></button>
+        </td>
         </tr>";
     }
     echo "</table>";
 } else {
     echo "<h4>No hay usuarios cargados en la Base de Datos";
 }
+
 ?>
 
+    <a href="./homeAdministrador.php"><input type="submit" value="Volver" class="btn text- white btn-dark">
+        </input></a>
 </div>
 
 <?php
-include_once '../estructura/secciones/footer.php';
+include_once '../estructura/footer.php';
 ?>
