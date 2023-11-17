cc02065822046d522616de42e1ec6009f8be85ba
NicolasBorghese <nicolasborghese@hotmail.com>
2023-11-17 16:35:48 -0300

    Se arreglan direcciones al ingresar al menu

--- configuracion.php
+++ configuracion.php
@@ -7,7 +7,7 @@ header ("Cache-Control: no-cache, must-revalidate ");
 // CONFIGURACION APP//
 /////////////////////////////
 
-$PROYECTO ='PWD/PWD_TP_Final/';
+$PROYECTO ='PWD_TP_Final/';
 
 //variable que almacena el directorio del proyecto
 $ROOT =$_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
